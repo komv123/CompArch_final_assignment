d7de628f43c19d1ce43ba2b996ceaee4a4e19f1f
WeihaoMo1 <weihaomo520@gmail.com>
2025-11-03 16:51:48 +0100

    Some cases in the branch are working

--- simsource/main.c
+++ simsource/main.c
@@ -20,75 +20,90 @@ gcc simsource/main.c -o main
 */
 
 uint32_t x[32]; // Registers
-uint32_t PC; // TODO: Implementing PC
+uint32_t PC;    // TODO: Implementing PC
 
-void dump_registers(){
-    for (short i = 0; i < 32; i++){
+void dump_registers()
+{
+    for (short i = 0; i < 32; i++)
+    {
         printf("x%d = 0x%08x\n", i, x[i]);
     }
 }
 
 /* -------------------- Instruction formats -------------------- */
-void r_type_executioner(uint32_t instr){
+void r_type_executioner(uint32_t instr)
+{
     uint8_t func3, func7;
     uint8_t rd, rs1, rs2;
     uint8_t opcode;
     uint8_t shamt;
 
-    opcode  = instr & 0x0000007F;
-    rd      = (instr >> 7) & 0x0000001F;
-    func3   = (instr >> 12) & 0x00000007;
-    rs1     = (instr >> 15) & 0x0000001F;
-    rs2     = (instr >> 20) & 0x0000001F;
-    func7   = (instr >> 25) & 0x0000007F;
+    opcode = instr & 0x0000007F;
+    rd = (instr >> 7) & 0x0000001F;
+    func3 = (instr >> 12) & 0x00000007;
+    rs1 = (instr >> 15) & 0x0000001F;
+    rs2 = (instr >> 20) & 0x0000001F;
+    func7 = (instr >> 25) & 0x0000007F;
 
-    printf("Instr: %08x, Opcode: %x, rd: %d, func3: 0x%x, rs1: %d, rs2: %d, func7: 0x%x", 
-        instr, opcode, rd, func3, rs1, rs2, func7);
-    
+    printf("Instr: %08x, Opcode: %x, rd: %d, func3: 0x%x, rs1: %d, rs2: %d, func7: 0x%x",
+           instr, opcode, rd, func3, rs1, rs2, func7);
+
     switch (opcode)
     {
     case 0x33: // Bitwise functions
         switch (func3)
         {
         case 0x0:
-            if (func7 == 0x00) {x[rd] = x[rs1] + x[rs2];} //add
-            else if (func7 == 0x20) {x[rd] = x[rs1] - x[rs2];} //sub
-            else {
+            if (func7 == 0x00)
+            {
+                x[rd] = x[rs1] + x[rs2];
+            } // add
+            else if (func7 == 0x20)
+            {
+                x[rd] = x[rs1] - x[rs2];
+            } // sub
+            else
+            {
                 printf("\nError in R-type. Exiting... \n\n");
                 dump_registers();
                 exit(EXIT_FAILURE);
             }
-            break;    
+            break;
         case 0x4:
-            x[rd] = x[rs1] ^ x[rs2]; //xor
+            x[rd] = x[rs1] ^ x[rs2]; // xor
             break;
         case 0x6:
-            x[rd] = x[rs1] | x[rs2]; //or
+            x[rd] = x[rs1] | x[rs2]; // or
             break;
         case 0x7:
-            x[rd] = x[rs1] & x[rs2]; //and
+            x[rd] = x[rs1] & x[rs2]; // and
             break;
         case 0x1:
             shamt = x[rs2] & 0x1F;
-            x[rd] = x[rs1] << shamt; //sll
+            x[rd] = x[rs1] << shamt; // sll
             break;
         case 0x5:
-            shamt = x[rs2] & 0x1F; //make sure that max shift is 31
-            if(func7 == 0x00) {
-                x[rd] = x[rs1] >> shamt; //srl
-            } else if (func7 == 0x20) {
-                x[rd] = (int32_t)x[rs1] >> shamt; //sra
-            } else {
+            shamt = x[rs2] & 0x1F; // make sure that max shift is 31
+            if (func7 == 0x00)
+            {
+                x[rd] = x[rs1] >> shamt; // srl
+            }
+            else if (func7 == 0x20)
+            {
+                x[rd] = (int32_t)x[rs1] >> shamt; // sra
+            }
+            else
+            {
                 printf("\nError in R-type. Exiting... \n\n");
                 dump_registers();
                 exit(EXIT_FAILURE);
             }
             break;
-        case 0x2: 
-            x[rd] = ((int32_t)x[rs1] < (int32_t)x[rs2]) ? 1 : 0; //slt
+        case 0x2:
+            x[rd] = ((int32_t)x[rs1] < (int32_t)x[rs2]) ? 1 : 0; // slt
             break;
         case 0x3:
-            x[rd] = (x[rs1] < x[rs2]) ? 1 : 0; //sltu
+            x[rd] = (x[rs1] < x[rs2]) ? 1 : 0; // sltu
             break;
         default:
             break;
@@ -106,7 +121,8 @@ void r_type_executioner(uint32_t instr){
     }
 }
 
-void i_type_executioner(uint32_t instr){
+void i_type_executioner(uint32_t instr)
+{
     uint8_t func3;
     int32_t imm;
     uint8_t rd, rs1;
@@ -114,17 +130,20 @@ void i_type_executioner(uint32_t instr){
     uint8_t shamt;
 
     /* Disassembly */
-    opcode  = instr & 0x0000007F;
-    rd      = (instr >> 7) & 0x0000001F;
-    func3   = (instr >> 12) & 0x00000007;
-    rs1     = (instr >> 15) & 0x0000001F;
-    imm     = (instr >> 20) & 0x00000FFF;
+    opcode = instr & 0x0000007F;
+    rd = (instr >> 7) & 0x0000001F;
+    func3 = (instr >> 12) & 0x00000007;
+    rs1 = (instr >> 15) & 0x0000001F;
+    imm = (instr >> 20) & 0x00000FFF;
 
     /* imm conversion from 12 bit to 32 bit */
-    if (imm & 0x800) {imm = 0xFFFFF000 | imm;}
+    if (imm & 0x800)
+    {
+        imm = 0xFFFFF000 | imm;
+    }
 
-    printf("Instr: %08x, Opcode: %x, rd: %d, func3: 0x%x, rs1: %d, imm: %x", 
-        instr, opcode, rd, func3, rs1, imm);
+    printf("Instr: %08x, Opcode: %x, rd: %d, func3: 0x%x, rs1: %d, imm: %x",
+           instr, opcode, rd, func3, rs1, imm);
 
     /* Execution */
     switch (opcode)
@@ -135,31 +154,34 @@ void i_type_executioner(uint32_t instr){
         case 0x0: // ADD Immediate
             x[rd] = x[rs1] + imm;
             break;
-        case 0x4: //xori
+        case 0x4: // xori
             x[rd] = x[rs1] ^ imm;
             break;
-        case 0x6: //ori
+        case 0x6: // ori
             x[rd] = x[rs1] | imm;
             break;
-        case 0x7: //andi
+        case 0x7: // andi
             x[rd] = x[rs1] & imm;
             break;
-        case 0x1: //slli
+        case 0x1: // slli
             shamt = imm & 0x1F;
             x[rd] = x[rs1] << shamt;
             break;
-        case 0x5: 
+        case 0x5:
             shamt = imm & 0x1F;
-            if(imm & 0x400) {  //1 << 10 (binary 0100 0000 0000)
-                x[rd] = (int32_t)x[rs1] >> shamt; //srai
-            } else {
-                x[rd] = x[rs1] >> shamt; //srli
+            if (imm & 0x400)
+            {                                     // 1 << 10 (binary 0100 0000 0000)
+                x[rd] = (int32_t)x[rs1] >> shamt; // srai
+            }
+            else
+            {
+                x[rd] = x[rs1] >> shamt; // srli
             }
             break;
-        case 0x2: //slti
+        case 0x2: // slti
             x[rd] = ((int32_t)x[rs1] < imm) ? 1 : 0;
             break;
-        case 0x3: //sltiu
+        case 0x3: // sltiu
             x[rd] = (x[rs1] < (uint32_t)imm) ? 1 : 0;
             break;
         default:
@@ -174,14 +196,17 @@ void i_type_executioner(uint32_t instr){
         break;
 
     case 0x73: // Enviroment operations
-        if (!imm) { // ecall
+        if (!imm)
+        { // ecall
             printf("\nEnvironment call registered.\nDumping registers and exiting... \n\n");
             dump_registers();
             exit(EXIT_SUCCESS);
-        } else {
+        }
+        else
+        {
             // TODO: ebreak
         }
-        
+
         break;
 
     default:
@@ -192,7 +217,8 @@ void i_type_executioner(uint32_t instr){
     }
 }
 
-void s_type_executioner(uint32_t instr){
+void s_type_executioner(uint32_t instr)
+{
     uint8_t func3;
     uint8_t imm_upper, imm_lower;
     uint8_t rs1, rs2;
@@ -201,61 +227,90 @@ void s_type_executioner(uint32_t instr){
     opcode = instr & 0x0000007F;
 }
 
-void b_type_executioner(uint32_t instr){
+void b_type_executioner(uint32_t instr)
+{
     uint8_t func3;
-    uint8_t imm11, imm4_1, imm10_5, imm12, imm;
+    uint8_t imm11, imm4_1, imm10_5, imm12;
+    int32_t imm;
     uint8_t rs1, rs2;
     uint8_t opcode;
 
-    opcode      = instr & 0x0000007F;
-    imm11       = (instr >> 7) & 0x00000001;
-    imm4_1      = (instr >> 8) & 0x0000000F;
-    func3       = (instr >> 12) & 0x00000007;
-    rs1         = (instr >> 15) & 0x0000001F;
-    rs2         = (instr >> 20) & 0x0000001F;
-    imm10_5     = (instr >> 25) & 0x0000003F;
-    imm12       = (instr >> 31) & 0x00000001;
+    opcode = instr & 0x0000007F;
+    imm11 = (instr >> 7) & 0x00000001;
+    imm4_1 = (instr >> 8) & 0x0000000F;
+    func3 = (instr >> 12) & 0x00000007;
+    rs1 = (instr >> 15) & 0x0000001F;
+    rs2 = (instr >> 20) & 0x0000001F;
+    imm10_5 = (instr >> 25) & 0x0000003F;
+    imm12 = (instr >> 31) & 0x00000001;
 
-    switch (func3) {
-        case 0x0:
-            if(rs1 == rs2) {
-                PC += imm;
-            }
-            break;
-        case 0x1:
-            if(rs1 != rs2) {
-                PC += imm;
-            }
-            break;
-        case 0x4:
-            if(rs1 < rs2) {
-                PC += imm;
-            }
-            break;
-        case 0x5:
-            if(rs1 >= rs2) {
-                PC += imm;
-            }
-            break;
-        case 0x6:
-            if(rs1 < rs2) {
-                PC += (uint32_t)imm;
-            }
-            break;
-        case 0x7:
-            if(rs1 >= rs2) {
-                PC += (uint32_t)imm;
-            }
-            break;
-        default: 
-            printf("\nError in I-type func3. Exiting... \n\n");
-            dump_registers();
-            exit(EXIT_FAILURE);
-            break;
+    imm = (imm12 << 12) |
+          (imm11 << 11) |
+          (imm10_5 << 5) |
+          (imm4_1 << 1);
+
+    if (imm & 0x800)
+    {
+        imm = 0xFFFFF000 | imm;
+    }
+
+    printf("Instr: %08x, Opcode: %x, func3: 0x%x, rs1: %d, imm: %d, rs2: %d, imm10_5: %x, imm4_1: %x, imm11: %x, imm12: %x",
+           instr, opcode, func3, rs1, imm, rs2, imm10_5, imm4_1, imm11, imm12);
+
+    switch (func3)
+    {
+    case 0x0:
+        if (x[rs1] == x[rs2])
+        {
+            PC += imm;
+            PC -= 4; // Adjust for the automatic PC increment after instruction fetch
+        }
+        break;
+    case 0x1:
+        if (x[rs1] != x[rs2])
+        {
+            PC += imm;
+            PC -= 4; // Adjust for the automatic PC increment after instruction fetch
+        }
+        break;
+    case 0x4:
+        if (x[rs1] < x[rs2])
+        {
+            PC += imm;
+            PC -= 4; // Adjust for the automatic PC increment after instruction fetch
+        }
+        break;
+    case 0x5:
+        if (x[rs1] >= x[rs2])
+        {
+            PC += imm;
+            PC -= 4; // Adjust for the automatic PC increment after instruction fetch
+        }
+        break;
+    case 0x6:
+        if (x[rs1] < x[rs2])
+        {
+            PC += (uint32_t)imm;
+            PC -= 4; // Adjust for the automatic PC increment after instruction fetch
+        }
+        break;
+    case 0x7:
+        if (x[rs1] >= x[rs2])
+        {
+            PC += (uint32_t)imm;
+            PC -= 4; // Adjust for the automatic PC increment after instruction fetch
+        }
+        break;
+    default:
+        printf("\nError in I-type func3. Exiting... \n\n");
+        dump_registers();
+        exit(EXIT_FAILURE);
+        break;
     }
 }
 
-void u_type_executioner(uint32_t instr){
+void u_type_executioner(uint32_t instr)
+{
     uint32_t imm;
     uint8_t rd;
     uint8_t opcode;
@@ -286,14 +341,16 @@ void u_type_executioner(uint32_t instr){
     }
 }
 
-void j_type_executioner(uint32_t instr){
+void j_type_executioner(uint32_t instr)
+{
     uint32_t imm;
     uint8_t rd;
     uint8_t opcode;
 }
 
 /* -------------------- Disassembler -------------------- */
-void instruction_disassembler(uint32_t instr){
+void instruction_disassembler(uint32_t instr)
+{
     uint8_t opcode = instr & 0x0000007F;
     printf("%x | ", opcode);
 
@@ -305,7 +362,7 @@ void instruction_disassembler(uint32_t instr){
     case 0x2F:
         r_type_executioner(instr);
         break;
-    
+
     /* I-types */
     case 0x13:
     case 0x03:
@@ -317,22 +374,22 @@ void instruction_disassembler(uint32_t instr){
     case 0x23:
         s_type_executioner(instr);
         break;
-    
+
     /* B-types */
     case 0x63:
         b_type_executioner(instr);
         break;
-    
+
     /* U-types */
     case 0x37:
         u_type_executioner(instr);
         break;
-    
+
     /* J-types */
     case 0x6F:
         j_type_executioner(instr);
         break;
-    
+
     default:
         printf("\nUnknown opcode: 0x%x\nExiting...\n\n", opcode);
         dump_registers();
@@ -341,10 +398,12 @@ void instruction_disassembler(uint32_t instr){
     }
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 
     /* Parse the command line arguments. */
-    if (argc != 2) {
+    if (argc != 2)
+    {
         printf("Usage:   %s <path to binary>\n", argv[0]);
         printf("Example: %s tests/task1/bool.bin\n", argv[0]);
         exit(EXIT_FAILURE);
@@ -353,23 +412,26 @@ int main(int argc, char* argv[]){
     /* Importing the binary */
     FILE *bin_file;
     bin_file = fopen(argv[1], "rb");
-    if (bin_file == NULL) {
-            printf("Error opening file\n");
-            exit(EXIT_FAILURE);
-        }
+    if (bin_file == NULL)
+    {
+        printf("Error opening file\n");
+        exit(EXIT_FAILURE);
+    }
     uint8_t bin[65536];
     size_t bytes_read = fread(bin, 1, 65536, bin_file);
 
     /* Reading the binary */
     uint32_t instr = 0;
-    for (PC = 0; PC < bytes_read; PC++) {
-        instr = (instr >> 8) + (bin[PC] << 24);  // Rearranging the bytes from LSB to MSB
-        if ((PC + 1) % 4 == 0) {
-            printf("\n%08x ", instr); 
+    for (PC = 0; PC < bytes_read; PC++)
+    {
+        instr = (instr >> 8) + (bin[PC] << 24); // Rearranging the bytes from LSB to MSB
+        if ((PC + 1) % 4 == 0)
+        {
+            printf("\n%08x ", instr);
             // Execute instruction
             instruction_disassembler(instr);
         }
-
+        printf("PC: %x", PC);
     }
     printf("\n");
     dump_registers();
